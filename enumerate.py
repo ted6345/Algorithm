18864b9f3420597a46f65ced11804f50ad07654a
2011003779 <ted6345@naver.com>
2021-02-15 17:52:42 +0900

    for문에서 enumerate 사용

---
+++ enumerate.py
@@ -0,0 +1,38 @@
+'''수포자는 수학을 포기한 사람의 준말입니다. 수포자 삼인방은 모의고사에 수학 문제를 전부 찍으려 합니다. 수포자는 1번 문제부터 마지막 문제까지 다음과 같이 찍습니다.
+
+1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
+2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
+3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
+
+1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때, 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 solution 함수를 작성해주세요.'''
+
+
+def solution(answers):
+    pattern1 = [1, 2, 3, 4, 5]
+    pattern2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    pattern3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    score = [0, 0, 0]
+    result = []
+
+    '''
+enumerate
+반복문 사용 시 몇 번째 반복문인지 확인이 필요할 수 있습니다. 이때 사용합니다.
+인덱스 번호와 컬렉션의 원소를 tuple형태로 반환합니다.
+'''
+
+    for idx, answer in enumerate(answers):
+        if answer == pattern1[idx % len(pattern1)]:
+            score[0] += 1
+        if answer == pattern2[idx % len(pattern2)]:
+            score[1] += 1
+        if answer == pattern3[idx % len(pattern3)]:
+            score[2] += 1
+
+    for idx, s in enumerate(score):
+        if s == max(score):
+            result.append(idx + 1)
+
+    return result
+
+
+
