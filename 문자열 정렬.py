bfff9bc4d519853225bd0b38c2dec12c78dec37a
2011003779 <ted6345@naver.com>
2021-02-16 16:09:32 +0900

    문자열 내림차순 및 join()추가

--- 문자열 정렬.py
+++ 문자열 정렬.py
@@ -48,3 +48,11 @@ def solution(strings, n):
         _answer.append(i[1:])
 
     return _answer
+
+'''문제 설명
+문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수, solution을 완성해주세요.
+s는 영문 대소문자로만 구성되어 있으며, 대문자는 소문자보다 작은 것으로 간주합니다.'''
+def solution(s):
+    # ''.join() 메서드로 list to string
+    answer = ''.join(sorted(s, reverse=True))
+    return answer
